601e194583d278d309f1c4cf707552f570e79e69
akhil-anand <80257983+akhil-anand@users.noreply.github.com>
2023-05-08 00:26:02 +0530

    update

--- src/routesConfig.js
+++ src/routesConfig.js
@@ -10,7 +10,7 @@ const RoutesConfig = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/Collections" element={<RecipeCollection />} />
         <Route path="/Recipe-page" element={<RecipePage />} />
-        <Route path="*" element={<Navigate to="/recipe-book" />} />
+        <Route path="*" element={<Navigate to="/recipe-book/" />} />
     </Routes>
   )
 }
