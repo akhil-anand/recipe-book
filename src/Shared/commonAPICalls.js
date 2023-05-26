aa1b6c69927aa36362dbb377a64676c92b23185c
akhil-anand <80257983+akhil-anand@users.noreply.github.com>
2023-05-26 20:16:46 +0530

    update

--- src/Shared/commonAPICalls.js
+++ src/Shared/commonAPICalls.js
@@ -6,7 +6,7 @@ export const getAllExistingRecipes = () => {
     return axios.get('https://drab-cyan-jellyfish-wrap.cyclic.app/api/allRecipes')
         .then((response) => {
             if (response.status === 200) {
-                store.dispatch(SET_RECIPE_LIST(response.data))
+                store.dispatch(SET_RECIPE_LIST(response.data.reverse()))
             }
         })
         .catch(error => console.log(`Error retrieving data: ${error}`))
